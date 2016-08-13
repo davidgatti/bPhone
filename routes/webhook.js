66256cd81aacafdf18c1ffd3c60ab226888fa04d
David Gatti <office@gatti.pl>
2016-08-13 22:20:29 +0200

    replaced

--- routes/webhook.js
+++ routes/webhook.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var twilio = require('twilio');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.post('/', function(req, res, next) {
 
     console.log(req.body);
 
