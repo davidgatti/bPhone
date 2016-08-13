f6c08a1f1f89593303334123f4c6e106094a68d2
David Gatti <office@gatti.pl>
2016-08-13 22:40:10 +0200

    Test webhook

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,12 +1,21 @@
 var express = require('express');
 var router = express.Router();
-var twilio = require('twilio');
+var client = require('twilio')(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
 
-    console.log(req.body);
-    res.end();
+    console.log(req.body.SmsMessageSid);
+
+
+    client.messages(req.body.SmsMessageSid).get(function(err, response) {
+
+            console.log(response);
+
+    });
+
+    res.status(200);
+    res.end('OK');
 
 });
 
