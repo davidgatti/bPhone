39697dcbb826bb5402e322013aa5b9cc009a588f
David Gatti <office@gatti.pl>
2016-09-14 18:46:42 +0200

    Better webhook handeling

--- routes/webhook.js
+++ routes/webhook.js
@@ -12,17 +12,25 @@ router.post('/', function(req, res, next) {
 		from: req.body.From.slice(1)
 	};
 
+	//
+	//	Query Twilio to retrieve the content of the received message
+	//
 	client.messages(req.body.SmsMessageSid).get(function(err, message) {
 
+		//
+		//	Send to the front-end the new message
+		//
     	process.bphone.emit('message', {
             date: message.dateCreated,
             body: message.body
-          });
+        });
+
+		//
+		//	Highlight the number that sent us the new message.
+		//
+		process.bphone.emit('newMessage', obj);
 
 	});
-
-	process.bphone.emit('alert', 'New Message to: ' + req.body.To);
-	process.bphone.emit('newMessage', obj);
 
     res.status(200);
     res.end('OK');
